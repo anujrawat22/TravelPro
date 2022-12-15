246ac2d8be5d2d5951aa72fc1fe0e626987d538c
dekta <ektadhawal6306@gmail.com>
2022-12-15 12:28:06 +0530

    footer

---
+++ frontend/components/footer.js
@@ -0,0 +1,135 @@
+function footer(){
+    return  `<div id="vs-footer">
+    <div id="vs-footer-con">
+        <img
+            src="https://a.travel-assets.com/globalcontrols-service/content/f285fb631b0a976202ef57611c7050e9ef5ca51a/images/EG_Wordmark_blue_RGB.svg"
+            alt=""
+          />
+      <ul id="vs-footer-ul">
+        <li>
+          <h2>Company</h2>
+          <ul>
+            <li>
+              <div><a href="">About</a></div>
+            </li>
+            <li>
+              <div><a href="">Jobs</a></div>
+            </li>
+            <li>
+              <div><a href="">List your property</a></div>
+            </li>
+            <li>
+              <div><a href="">Partnerships</a></div>
+            </li>
+            <li>
+              <div><a href="">Newsroom</a></div>
+            </li>
+            <li>
+              <div><a href="">Investor Relations</a></div>
+            </li>
+            <li>
+              <div><a href="">Site Map</a></div>
+            </li>
+            <li>
+              <div><a href="">TravelPro Rewards</a></div>
+            </li>
+            <li>
+              <div><a href="">Advertising</a></div>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h2>Explore</h2>
+          <ul>
+            <li>
+              <div><a href="">Hotels in United States</a></div>
+            </li>
+            <li>
+              <div><a href="">Vacation REntals in United States</a></div>
+            </li>
+            <li>
+              <div><a href="">Car Rentals in United States</a></div>
+            </li>
+            <li>
+              <div><a href="">Domestic Flights</a></div>
+            </li>
+            <li>
+              <div><a href="">Vacation Packages in United States</a></div>
+            </li>
+            <li>
+              <div><a href="">TravelPro Reviews</a></div>
+            </li>
+            <li>
+              <div><a href="">TravelPro Coupons</a></div>
+            </li>
+            <li>
+              <div><a href="">LGBTQ Travel</a></div>
+            </li>
+            <li>
+              <div><a href="">Unique Accommodations</a></div>
+            </li>
+            <li>
+              <div><a href="">Travel Blog</a></div>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h2>Policies</h2>
+          <ul>
+            <li>
+              <div><a href="">Privacy Policy</a></div>
+            </li>
+            <li>
+              <div><a href="">Terms of Use</a></div>
+            </li>
+            <li>
+              <div><a href="">Vrbo terms and conditions</a></div>
+            </li>
+            <li>
+              <div><a href="">TravelPro Rewards Terms</a></div>
+            </li>
+            <li>
+              <div><a href="">Do not sell my personal information</a></div>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h2>Help</h2>
+          <ul>
+            <li>
+              <div><a href="">Support</a></div>
+            </li>
+            <li>
+              <div>
+                <a href="">Cancel your hotel or vacation rental booking</a>
+              </div>
+            </li>
+            <li>
+              <div><a href="">Cancel your flight</a></div>
+            </li>
+            <li>
+              <div>
+                <a href="">Refund timelines, policies & processes</a>
+              </div>
+            </li>
+            <li>
+              <div><a href="">Use an TravelPro Coupon</a></div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <hr />
+      <ul id="vs-bottom-ul">
+        <li>
+          © 2022 TravelPro, LLC, an Expedia Group Company. All rights reserved.
+        </li>
+        <li>
+          Travel Pro, TravelPro.com, and the TravelPro logo are trademarks or
+          registered trademarks of TravelPro, LLC. CST# 2083930-50.
+        </li>
+      </ul>
+    </div>
+  </div>
+  `
+  }
+  export default footer;
