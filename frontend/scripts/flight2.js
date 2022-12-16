fbbb3ce33098c8a3956e0dbc67289d549df452ca
priyanshu2014204 <priyanshu2014204@gmail.com>
2022-12-16 21:21:13 +0530

    day_3 commit

---
+++ frontend/scripts/flight2.js
@@ -0,0 +1,2 @@
+document.querySelector(".leaving").innerText=JSON.parse(localStorage.getItem("leaving_place"))
+document.querySelector(".going").innerText=JSON.parse(localStorage.getItem("going_place"))
