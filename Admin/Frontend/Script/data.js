b9f79bb74d1cedf77d826bf6733feb3fbb74f79d
pratap562 <kumpratap562@gmail.com>
2022-12-17 17:43:07 +0530

    data sending to backend via admin work on progress

--- Admin/Frontend/Script/data.js
+++ Admin/Frontend/Script/data.js
@@ -1,18 +1,29 @@
+const sendData = {}
+
 let takeData = (obj) => {
     for (let keys in obj) {
-        if (keys == "rooms" || keys == "rating" || keys == "star_rating" || keys == "review" || keys == "review_count") {
-            console.log(keys);
+        if (keys == "rooms" || keys == "rating" || keys == "star_rating" || keys == "review" || keys == "_id" || keys == "review_count" || keys == "userID" || keys == "__v") {
+            console.log(keys, 'special');
             continue
         }
-        let tgName = document.querySelector(`#${keys} > label`).children[1].tagName
+        console.log(keys);
+        let tgName = document.querySelector(`#${keys} > label`)?.children[1].tagName
+        let val = document.querySelector(`#${keys} > label`)?.children[1].value
         console.log(tgName);
+        if (tgName == 'TEXTAREA') {
+            val = val.split('\n')
+        }
+        sendData[keys] == val
+        console.log(val);
+
     }
+
 }
 
 let fetching = async () => {
     let data = await fetch('https://sore-plum-spider-hem.cyclic.app/hotels')
     data = await data.json()
     console.log(data);
-    takeData(data)
+    takeData(data[0])
 }
 fetching()
