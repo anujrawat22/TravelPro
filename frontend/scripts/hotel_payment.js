1a1af00422f16c72c7255ac9446d4aa975b6df0b
dekta <ektadhawal6306@gmail.com>
2022-12-18 01:14:48 +0530

    payment page

---
+++ frontend/scripts/hotel_payment.js
@@ -0,0 +1,17 @@
+function show1() {
+    var x = document.getElementById("hide1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function show2() {
+    var x = document.getElementById("hide2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
