822aed2b67b1f41a40227b2feb2d9e4d8986b0a7
priyanshu2014204 <priyanshu2014204@gmail.com>
2022-12-17 20:27:34 +0530

    saved

--- frontend/scripts/flight2.js
+++ frontend/scripts/flight2.js
@@ -1,2 +1,3 @@
-document.querySelector(".leaving").innerText=JSON.parse(localStorage.getItem("leaving_place"))
-document.querySelector(".going").innerText=JSON.parse(localStorage.getItem("going_place"))
+    document.querySelector(".leaving").innerText=JSON.parse(localStorage.getItem("leaving_place"));
+    document.querySelector(".going").innerText=JSON.parse(localStorage.getItem("going_place"));
+
