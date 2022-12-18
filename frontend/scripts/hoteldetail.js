e9c0ab227263217dcf695c7ac544877173c761fa
anujrawat22 <anuj22rawat20@gmail.com>
2022-12-18 17:01:30 +0530

    hotel details saved

--- frontend/scripts/hoteldetail.js
+++ frontend/scripts/hoteldetail.js
@@ -1,8 +1,9 @@
 
 import { navbar } from "../components/navbar.js";
+import barcode from "../components/bar_code.js";
 
 document.getElementById("navbar").innerHTML = navbar()
-
+document.getElementById("barcode").innerHTML = barcode()
 let hoteldata = JSON.parse(localStorage.getItem("hoteldata")) || {}
 
 console.log(hoteldata)
@@ -103,9 +104,80 @@ function roomcards(arr){
 
       div1.append(img)
     
-      div.append(div1)
       
+      
+      let div2 = document.createElement("div")
+      div2.setAttribute("class","hotel_name")
+      let hotelname = document.createElement("p")
+      hotelname.innerText = el.room_title
+      div2.append(hotelname)
+      
+     let div3 = document.createElement("div")
+     div3.setAttribute("class","roomsize")
+     let area = document.createElement("p")
+     area.innerText = el.room_size
+     div3.append(area)
+     
+
+     let div4 = document.createElement("div")
+     div4.setAttribute("class","reserveandpay")
+     let reservetext = document.createElement("p")
+     reservetext.innerText = "Reserve now, pay later"
+     div4.append(reservetext)
+     
+     let div5 = document.createElement("div")
+     div5.setAttribute("class","moredetails")
+     let moredetails = document.createElement("p")
+     moredetails.innerText= "More details"
+     div5.append(moredetails)
+
+     let div6 = document.createElement("div")
+     div6.setAttribute("class","reservediv")
+     
+     let price = document.createElement("p")
+     price.setAttribute("class","roomprice")
+     price.innerText = `Price -$${el.room_price}`
+     
+      let reservebtn = document.createElement("button")
+      reservebtn.setAttribute("class","reservebtn")
+      reservebtn.innerText = "Reserve"
+      reservebtn.addEventListener("click",()=>{
+        reserveroom(el)
+      })
+      div6.append(price,reservebtn)
+      
+
+      let div7 = document.createElement("div")
+      div7.setAttribute("class","availability")
+      let availability = document.createElement("p")
+      if(el.is_available){
+        availability.innerText = "Available"
+        availability.style.color = "green"
+      }else{
+        availability.innerText = "Currently Not available"
+        availability.style.color = "red"
+      }
+      div7.append(availability)
+
+      div.append(div1,div2,div3,div4,div5,div7,div6)
       hotelroomcard.append(div)
 
     })
+}
+
+
+// function for adding room details to local storage
+
+function reserveroom(el){
+    
+    let checkin = document.getElementById("datepicker1").value
+    let checkout = document.getElementById("datepicker2").value
+    let rooms = +document.getElementById("rooms").value
+    let travelers = +document.getElementById("travelers").value
+    el.checkin = checkin
+    el.checkout = checkout
+    el.no_of_rooms= rooms
+    el.travelers = travelers
+
+    localStorage.setItem("roomsdata",JSON.stringify(el))
 }
